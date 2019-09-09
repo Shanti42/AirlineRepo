ef4a4451cdce7ee47ef224d66ce26cbd0a244554
Shanti Polara <smp163@case.edu>
2019-09-08 21:45:20 -0400

    Add Airport class

---
+++ src/airtravel/Airport.java
@@ -0,0 +1,63 @@
+package airtravel;
+
+import java.time.Duration;
+import java.util.Comparator;
+import java.util.Objects;
+
+public final class Airport implements Comparable<Airport> {
+
+    // short string identifier for the Airport
+    private final String code;
+    // the chortest length of time for a passenger to transfer planes
+    private final Duration connectionTimeMin;
+
+    private Airport (String code, Duration connectionTimeMin){
+        this.code = code;
+        this.connectionTimeMin = connectionTimeMin;
+    }
+
+    public static final Airport of(String code, Duration connectionTimeMin){
+        if(code == null) {
+            throw new NullPointerException("Airport identifier code is null");
+        } else if (connectionTimeMin == null){
+            throw new NullPointerException("Connection time parameter is null");
+        } else {
+            return new Airport(code, connectionTimeMin);
+        }
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public Duration getConnectionTimeMin(){
+        return connectionTimeMin;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        boolean equals = false;
+        if(object != null && object instanceof Airport) {
+            Airport otheraAirport = (Airport) object;
+            equals = getCode().equals(otheraAirport.code);
+        }
+        //else: equals is false, but value already assigned
+
+        return equals;
+    }
+
+    @Override
+    public int hashCode() {
+        return getCode().hashCode();
+    }
+
+    @Override
+    public int compareTo(Airport otherAirport) {
+        return getCode().compareTo(otherAirport.getCode());
+    }
+
+    @Override
+    public String toString() {
+        return getCode();
+    }
+}
