01da37d47bbb71709423765b6f6ed2ff82aeffc1
Shanti Polara <smp163@case.edu>
2019-09-11 19:24:24 -0400

    Update AirTravelTest.java

---
+++ src/airtravel/tests/AirTravelTest.java
@@ -0,0 +1,49 @@
+package airtravel.tests;
+
+import airtravel.Airport;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirTravelTest {
+
+    private AirTravelTest(){
+        Duration hour1 = Duration.ofHours(1);
+        Duration hour2 = Duration.ofHours(2);
+        Duration hour3 = Duration.ofHours(3);
+        final String CLE = "CLE";
+        final String
+
+        Object notAirport = new Object();
+        Airport a1 = 
+    }
+
+
+    @Test
+    void of() {
+    }
+
+    @Test
+    void getCode() {
+    }
+
+    @Test
+    void getConnectionTimeMin() {
+    }
+
+    @Test
+    void equals() {
+
+    }
+
+    @Test
+    void hashCode() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+
+}
