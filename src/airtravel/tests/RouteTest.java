9d53ff5df4af89d75752f6eff3b1962c6fe78710
Jack Bilotti <jtb129@case.edu>
2019-09-24 18:57:33 -0400

    Update RouteTest.java

--- src/airtravel/tests/RouteTest.java
+++ src/airtravel/tests/RouteTest.java
@@ -1,16 +1,40 @@
 package airtravel.tests;
+import static airtravel.SeatClass.*;
 
+import airtravel.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class RouteTest {
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class RouteTest  {
 
 
+    public Airport airport1;
+    public Airport airport2;
+    public Airport airport3;
+    public Airport airport4;
+
+    @BeforeAll
+    void initializeRoutes() {
+        airport1 = Airport.of("CLE",Duration.ofHours(5));
+        airport2 = Airport.of("CLE",Duration.ofHours(7));
+        airport3 = Airport.of("CLE",Duration.ofHours(3));
+        airport4 = Airport.of("CLE",Duration.ofHours(1));
+
+    }
     /**
      * RouteNode Tests
      */
     @Test
     void testRouteNodeBuildMethods() {
+        Airport airport = Airport.of("CLE",Duration.ofHours(5));
+        Airport airport2 = Airport.of("CLE",Duration.ofHours(10));
+        RouteTime arrivalTime = new RouteTime(LocalTime.of(6,0));
 
+        RouteNode routeNode1 = RouteNode.of(airport, arrivalTime, null);
+        RouteNode routeNode2 = RouteNode.of(airport, arrivalTime)
     }
 
     @Test
