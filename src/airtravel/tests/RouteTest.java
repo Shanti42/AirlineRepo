79b76d4dce745c58f9042b96a1ddc1fa8deef5f4
Jack Bilotti <jtb129@case.edu>
2019-09-23 22:39:38 -0400

    Create RouteTest.java

---
+++ src/airtravel/tests/RouteTest.java
@@ -0,0 +1,63 @@
+package airtravel.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class RouteTest {
+
+
+    /**
+     * RouteNode Tests
+     */
+    @Test
+    void testRouteNodeBuildMethods() {
+
+    }
+
+    @Test
+    void testRouteNodeIsArrivalTimeKnown() {
+
+    }
+
+    @Test
+    void testRouteNodeDepartureTime() {
+
+    }
+
+    @Test
+    void testRouteNodeAvailableFlights() {
+
+    }
+
+    @Test
+    void testRouteNodeCompareTo() {
+
+    }
+
+    /**
+     * Test RouteState
+     */
+    @Test
+    void testRouteStateOf() {
+
+    }
+
+    @Test
+    void testRouteStateReplaceNode() {
+
+    }
+
+    @Test
+    void testRouteStateClosestUnreached() {
+
+    }
+
+    /**
+     * RouteFinder
+     */
+
+    @Test
+    void testRouteFinderRoute() {
+
+    }
+
+}
